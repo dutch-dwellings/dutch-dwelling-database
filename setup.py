293b22a102443617fb83dc9d1f5ef7da05529516
Rens Baardman <git@rensbaardman.nl>
2021-06-29 19:33:33 +0200

    Remove old setup statements

--- setup.py
+++ setup.py
@@ -22,9 +22,6 @@ from utils.CBS_PC6_2017_kerncijfers_create_table import main as create_CBS_PC6_2
 from utils.CBS_PC6_2017_kerncijfers_download import main as download_CBS_PC6_2017_kerncijfers
 from utils.CBS_PC6_2017_kerncijfers_load import main as load_CBS_PC6_2017_kerncijfers
 
-from utils.verhuur_folckert_create_table import main as create_verhuur_folckert_table
-from utils.verhuur_folckert_load import main as load_verhuur_folckert
-
 from utils.elec_consumption_hh_create_table import main as create_elec_consumption_households_table
 from utils.elec_consumption_hh_load import main as load_elec_consumption_households
 
@@ -41,10 +38,6 @@ def bag():
 	add_index('bag', 'pc6')
 	add_index('bag', 'pand_id')
 	add_index('bag', 'buurt_id')
-
-def verhuur_folckert():
-	create_verhuur_folckert_table()
-	load_verhuur_folckert()
 
 def CBS_PC6():
 	print('Creating table for CBS PC6...')
@@ -235,8 +228,6 @@ def main():
 
 	print('\n====== WoON ======')
 	load_WoON()
-
-	verhuur_folckert()
 
 	print('\nFinished with the setup.')
 
